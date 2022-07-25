2f034efcf141e2c94462ac9c5d51957420c53497
jimhuang <jimvinhuang@gmail.com>
2022-07-25 21:45:11 +0800

    feat: 🎸 [MyScreen.tsx] add MyScreen component

---
+++ src/MyScreen.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
+
+const MyScreen = () => {
+  const [status, setStatus] = useState('');
+
+  return (
+    <View>
+      <Text testID="myText">{status}</Text>
+      <Button
+        testID="myButton"
+        onPress={() => setStatus('button pressed')}
+        title="PressMe"
+      />
+    </View>
+  );
+};
+
+export default MyScreen;
